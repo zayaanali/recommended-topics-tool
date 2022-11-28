8ef4e006c4903a3b14f8dc4576f7c0d101ad5f45
Kevin C.C. Chang <benlkchang@gmail.com>
2022-11-28 00:50:17 -0600

    Added filter_edges, creates an adjacency list of the top 130,000 nodes and writes it to a file. Started the BFS file in main

---
+++ entry/filter_edges.cpp
@@ -0,0 +1,56 @@
+#include <map>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <unordered_set>
+#include<sstream>
+
+//creates and stores an adjacency list with only the top 130K nodes
+int main() {
+  static std::map<int, std::vector<int>> full_adj;
+  static std::map<int, std::vector<int>> cut_adj;
+  std::unordered_set<int> trimmed;
+  std::ifstream infile("../filteredwiki.txt");
+  std::ifstream infile2("../data/wikidata.txt"); //edge file
+  int from, to;
+  while (infile2 >> from >> to) {
+    full_adj[from].push_back(to);
+  }
+  std::cout << "MADE IT" << '\n';
+  std::string line;
+  while (infile) {
+    std::getline(infile, line);
+    std::stringstream s_stream(line);
+    size_t pos = line.find(',');
+    std::cout << line << '\n';
+    int idx = std::stoi(line.substr(0, pos));
+    trimmed.insert(idx);
+    if (idx == 131511) { //number of pages with more than 100 links to it
+      break;
+    }
+  }
+  std::cout << "Here" << '\n';
+  std::ofstream Writing("../data/filterededges.txt");
+  for (auto itr = trimmed.begin(); itr != trimmed.end(); itr++) {
+    std::vector<int> adjacent = full_adj[*itr];
+    std::vector<int> newadj;
+    for (int v: adjacent) {
+      if (trimmed.find(v) != trimmed.end()) {
+        newadj.push_back(v);
+      }
+    }
+    std::string adding;
+    for (int to: newadj) {
+      adding += std::to_string(to) + ","; 
+    }
+    if (adding.length() > 0) {
+      adding.pop_back();
+    }
+    Writing << *itr << ',' << adding << '\n';
+    cut_adj[*itr] = newadj;
+  }
+  Writing.close();
+}
--- entry/main.cpp
+++ entry/main.cpp
@@ -5,47 +5,19 @@
 #include <algorithm>
 #include <utility>
 #include <string>
+#include <unordered_set>
 #include<sstream>
-
-bool compare(const std::pair<int,int> &a,const std::pair<int,int> &b) {
-  return a.second > b.second;
-}
+//BFS FILE
 int main() {
-  static std::map<int, int> log;
-  static std::map<int,std::string> titles;
-  std::vector<std::pair<int,int>> filtered;
-  std::ifstream infile("data/wikidata.txt"); //edge file
-  std::ifstream infile2("data/enwiki-2013-names.csv"); //titles file
+  static std::map<int, std::vector<int>> adj;
+  std::ifstream infile("../data/filterededges.txt"); //edge file
+  //TODO Load the graph from filterededges.txt (run filter_data and filter_edges)
   std::string line;
-  while (infile2) {
-    std::getline(infile2, line);
-    std::stringstream s_stream(line);
-    size_t pos = line.find(',');
-    std::cout << line << '\n';
-    int idx = std::stoi(line.substr(0, pos));
-    line.erase(0, pos + 1);
-    titles[idx] = line;
-    if (idx == 4206784) { //number of wikipedia pages
+  while (infile) {
+    std::getline(infile, line);
+    
+    if (idx == 131511) { //number of pages with more than 100 links to it
       break;
     }
   }
-  int from, to;
-  while (infile >> from >> to) {
-    log[to]++;
-  }
-  for (auto i = log.begin(); i != log.end(); i++) {
-    if (i->second > 100) {
-      filtered.push_back(std::make_pair(i->first, i->second));
-      std::cout << i->first << ' ' << i->second << '\n';
-    }
-  }
-  std::sort(filtered.begin(), filtered.end(), compare);
-  std::ofstream Writing("filteredwiki.txt");
-  for (auto i: filtered) {
-    // Writing << i.first << '\n';
-    Writing << i.first << ',' << titles[i.first] << '\n';
-  }
-  Writing.close();
-  std::cout << "SIZE OF FILTERED IS: " << filtered.size() << '\n';
-
 }
