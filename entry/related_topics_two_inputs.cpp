2bf1bd48eec7a7e2f23e22efcfa5cf5be8c3e060
Kevin C.C. Chang <benlkchang@gmail.com>
2022-12-09 18:33:57 -0600

    finalized related topics code

--- entry/related_topics_two_inputs.cpp
+++ entry/related_topics_two_inputs.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <unordered_set>
 #include<sstream>
+#include<cmath>
 #include <queue>
 #include "utils.h"
 #include "graph.h"
@@ -16,11 +17,16 @@
  * when there are multiple predecessors to choose from.
 */
 std::map<int,double> betweenness;
+double closest = 0;
 bool compareB(const int &a,const int &b) {
   return betweenness[a] < betweenness[b];
 }
-
+bool closeB(const int &a,const int &b) {
+  return std::abs(betweenness[a] - closest) < std::abs(betweenness[b] - closest);
+}
 int main(int argc, char** argv) {
+
+  //check input
   if (argc < 3) {
     std::cout << "You did not enter enough arguments. Please enter the indexes of two wikipedia pages" << '\n';
     return 0;
@@ -42,21 +48,50 @@ int main(int argc, char** argv) {
     std::cout << "The second argument is not a valid Wikipedia page title. Please try again." << '\n';
     return 0;
   }
-  int topic1 = reverse_titles[argv[1]]; //741034 -> piano
-  int topic2 = reverse_titles[argv[2]]; //605008 -> France
+
+  //convert strings to node indexes
+  int topic1 = reverse_titles[argv[1]]; 
+  int topic2 = reverse_titles[argv[2]]; 
   betweenness = load_betweenness("../data/betweenness.txt", graph, 15470);
   
+  //find shortest paths to topic 1
   std::map<int, std::vector<int>> predecessor = graph.brandes_predecessor(topic1);
   int current = topic2;
-  std::vector<int> street;
+
+  std::vector<std::vector<int>> street;
   while (current != topic1) {
     std::vector<int> options = predecessor[current];
+    if (options.size() == 0) {
+      break;
+    }
     std::sort(options.begin(), options.end(), compareB);
-    street.push_back(options[options.size() / 2]); //the full path
+    std::vector<int> collection;
+    if (options[options.size() / 2] != topic1) {
+      unsigned int median = options.size() / 2;
+      collection.push_back(options[median]); //pushing back the 3 predecessors with the most median betweenness centrality scores to a vector with the full path
+      if (median + 1 < options.size()) {
+        collection.push_back(options[median + 1]);
+      }
+      if (median != 0) { //can't use -1 since median will overflow
+        collection.push_back(options[median - 1]);
+      }
+    }
     current = options[options.size() / 2]; // picks the node with the middle betweenness centrality if there are multiple predecessors
+    if (collection.size() > 0) {
+      street.push_back(collection);
+    }
   }
   std::cout << "~~~~~" << '\n';
-  for (int stop: street) {
+  if (street.size() == 0) {
+    closest = betweenness[topic2];
+    std::vector<int> neighbors = graph.getAdjacent(topic1);
+    std::sort(neighbors.begin(), neighbors.end(), closeB);
+    std::cout << "Direct link exists, finding a neighbor of " << titles[topic1] << " similar to " << titles[topic2] << '\n';
+    std::cout << titles[neighbors[1]] << '\n';
+  } else {
+    for (int stop: street[street.size() / 2]) { //goes to the middle group of predecessors on the path, should be most related to both topics
     std::cout << titles[stop] << '\n';
+    }
   }
+  std::cout << "~~~~~" << '\n';
 }
